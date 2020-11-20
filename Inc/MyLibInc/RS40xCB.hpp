66bdf529e63907e76c7341520ddf1132a3739442
tenchou <49236225+tenchou89@users.noreply.github.com>
2020-11-20 16:14:23 +0900

    マージするライブラリに合わせて再開発
    実験はまだしていない

--- Inc/MyLibInc/RS40xCB.hpp
+++ Inc/MyLibInc/RS40xCB.hpp
@@ -14,16 +14,76 @@
 
 class RS40xCB{
 public:
+
+    enum class memory_adr :const uint8_t{
+		ID = 0x04,
+		BAUD_RATE = 0x06,
+		RETURN_DELAY,
+		CW_ANGLE_LIMIT_L,
+		CW_ANGLE_LIMIT_H,
+		CCW_ANGLE_LIMIT_L,
+		CCW_ANGLE_LIMIT_H,
+		TEMPERATURE_LIMIT_L = 0x0E,
+		TEMPERATURE_LIMIT_H,
+		DAMPER = 0x14,
+		TORQUE_IN_SILENCE = 0x16,
+		WARM_UP_TIME,
+		CW_COMPLIANCE_MARGIN,
+		CCW_COMPLIANCE_MARGIN,
+		CW_COMPLIANCE_SLOPE,
+		CCW_COMPLIANCE_SLOPE,
+		PUNCH_L,
+		PUNCH_H,
+		/*RAM*/
+		GOAL_POSITION_L,
+		GOAL_POSITION_H,
+		GOAL_TIME_L,
+		GOAL_TIME_H,
+		MAX_TORQUE = 0x23,
+		TorqueOnOff,
+		PRESENT_POSITION_L = 0x2A,
+		PRESENT_POSITION_H,
+		PRESENT_TIME_L,
+		PRESENT_TIME_H,
+		PRESENT_SPEED_L,
+		PRESENT_SPEED_H,
+		PRESENT_CURRENT_L,
+		PRESENT_CURRENT_H,
+		PRESENT_TEMPERATURE_L,
+		PRESENT_TEMPERATURE_H,
+		PRESENT_VOLTS_L,
+		PRESENT_VOLTS_H,
+		SPECIAL_OPERATION = 0xFF
+    };
+
+    enum class flags : uint8_t{
+		RETURN_ADR_1 = 0x01,
+		RETURN_ADR_2 = 0x02,
+		RETURN_ADR_3 = 0x04,
+		RETURN_ADR_4 = 0x08,
+		RETURN_ADR_ALL = 0x0F,
+		MEMORY_INITIALIZE = 0x10,
+		REBOOT = 0x20,
+		WRITE_FLASH_ROM = 0x40
+	};
+    enum class return_flags : uint8_t{
+		READ_PACET_ERROR = 0x02,
+		WRITE_ROM_ERROR = 0x08,
+		TEMPERATURE_LIMIT_ARRAM = 0x20,
+		TEMPERATURE_LIMIT_ERROR = 0x80
+	};
+
 	RS40xCB(const IOPin io);
 	~RS40xCB();
 	void Init(UART_HandleTypeDef* uarthandle);
-	void send_data(uint8_t id, uint8_t adr, char data);
-	void SendDataOfShortpacket(const uint8_t id, const uint8_t address, const std::vector<uint8_t>& data);
-	void SendDataOfLongpacket(const std::vector<uint8_t> id,const uint8_t address, const std::vector<std::vector<uint8_t>> data);
+	void SendDataOfShortpacket(uint8_t id, memory_adr address, std::vector<uint8_t> data);
+	void SendDataOfLongpacket(std::vector<uint8_t> id, uint8_t address, std::vector<std::vector<uint8_t>> data);
+	void SpecialTransmissionData(uint8_t id, flags flag, memory_adr address);
 	void TorqueOn(std::vector<uint8_t>sendector_short_torque_on);
 	void TorqueOff(std::vector<uint8_t>sendector_short_torque_off);
-	void change_id(uint8_t old_id, char new_id);
 	void write_rom(uint8_t id);
+	void reboot(uint8_t id);
+	void change_id(uint8_t old_id, uint8_t new_id, std::vector<uint8_t> sendvector_new_id);
 
 
 	inline std::vector<uint8_t> get_shortpacketServoData(){return shortpacketServoData_;}
@@ -35,21 +95,13 @@ public:
     inline void clear_longpacketServoId(){longpacketServoId_.clear();}
     inline void clear_longpacketServoData(){longpacketServoData_.clear();}
 
-	//Header
-	uint8_t kHeader_HighByte = 0xFA;
-	uint8_t kHeader_LowByte = 0xAF;
+    //Header
+    uint8_t kHeader_HighByte = 0xFA;
+	uint8_t kHeader_LowByte  = 0xAF;
 	uint8_t kHeader_ALLSERVO = 0xFF;
-	//Adress
-	uint8_t kAdr_ID = 0x04;
-	uint8_t kAdr_GOALPOSITION_L = 0x1E;
-	uint8_t kAdr_TorqueOnOff = 0x24;
-	//Flags
-	uint8_t kFlag_WRITEFLASHROM = 0x40;
-	uint8_t kFlag_REBOOT = 0x20;
 	//Data
 	uint8_t kData_TorqueOn = 0x01;
 	uint8_t kData_torqueOff = 0x00;
-
 private:
 	std::vector<uint8_t> shortpacketServoData_;
 	std::vector<uint8_t> longpacketServoId_;
--- Src/RS40xCB.cpp
+++ Src/RS40xCB.cpp
@@ -20,8 +20,7 @@ void RS40xCB::Init(UART_HandleTypeDef* uarthandle){
 	uart_ = uarthandle;
 }
 
-
-void RS40xCB::SendDataOfShortpacket(const uint8_t id, const uint8_t address, const std::vector<uint8_t>& data){
+void RS40xCB::SendDataOfShortpacket(uint8_t id, memory_adr address, std::vector<uint8_t> data){
 	std::vector<uint8_t> packet;
 	packet.reserve(8 + data.size());
 
@@ -34,7 +33,7 @@ void RS40xCB::SendDataOfShortpacket(const uint8_t id, const uint8_t address, con
 	/*flags*/
 	packet.push_back(0);
 	/*address*/
-	packet.push_back(address);
+	packet.push_back(static_cast<uint8_t>(address));
 	/*length*/
 	packet.push_back(static_cast<uint8_t>(data.size()));
 	/*count*/
@@ -94,76 +93,62 @@ void RS40xCB::SendDataOfLongpacket(const std::vector<uint8_t> id,const uint8_t a
 	servoWriteRead_.write(false);
 }
 
-
-
-void RS40xCB::TorqueOn(std::vector<uint8_t>sendector_short_torque_on){
-	sendector_short_torque_on.push_back(kData_TorqueOn);
-	SendDataOfShortpacket(kHeader_ALLSERVO, kAdr_TorqueOnOff, sendector_short_torque_on);
-	sendector_short_torque_on.clear();
-}
-
-void RS40xCB::TorqueOff(std::vector<uint8_t>sendector_short_torque_off){
-	sendector_short_torque_off.push_back(kData_torqueOff);
-	SendDataOfShortpacket(kHeader_ALLSERVO, kAdr_TorqueOnOff, sendector_short_torque_off);
-	sendector_short_torque_off.clear();
-}
-
-void RS40xCB::change_id(uint8_t old_id, char new_id){
+void RS40xCB::SpecialTransmissionData(uint8_t id, flags flag, memory_adr address){
 	std::vector<uint8_t> packet;
-	packet.reserve(8 + sizeof(char));
+	packet.reserve(8);
 
 	/*header*/
 	packet.push_back(kHeader_HighByte);
 	packet.push_back(kHeader_LowByte);
 
 	/*servo id*/
-	packet.push_back(old_id);
+	packet.push_back(id);
 	/*flags*/
-	packet.push_back(0);
+	packet.push_back(static_cast<uint8_t>(flag));
 	/*address*/
-	packet.push_back(kAdr_ID);
+	packet.push_back(static_cast<uint8_t>(address));
 	/*length*/
-	packet.push_back(sizeof(char));
+	packet.push_back(0);
 	/*count*/
-	packet.push_back(1);
-	/*data*/
-	packet.resize(7 + sizeof(char));
-	memcpy(&packet[7], &new_id, sizeof(char));
-
+	packet.push_back(0);
 	/*sum*/
 	packet.push_back(0);
 	for(uint8_t i = 2;i < (packet.size() - 1);++i)
 		packet.back() ^= packet.at(i);
 
 	servoWriteRead_.write(true);
-	HAL_UART_Transmit(uart_, packet.data(), static_cast<uint16_t>(packet.size()), 100);
+	HAL_UART_Transmit(uart_ , packet.data(), static_cast<uint16_t>(packet.size()), 100);
 	servoWriteRead_.write(false);
+}
+
+
+void RS40xCB::TorqueOn(std::vector<uint8_t>sendvector_short_torque_on){
+	sendvector_short_torque_on.push_back(kData_TorqueOn);
+	SendDataOfShortpacket(kHeader_ALLSERVO, memory_adr::TorqueOnOff, sendvector_short_torque_on);
+	sendvector_short_torque_on.clear();
+}
+
+void RS40xCB::TorqueOff(std::vector<uint8_t>sendvector_short_torque_off){
+	sendvector_short_torque_off.push_back(kData_torqueOff);
+	SendDataOfShortpacket(kHeader_ALLSERVO, memory_adr::TorqueOnOff, sendvector_short_torque_off);
+	sendvector_short_torque_off.clear();
 }
 
 void RS40xCB::write_rom(uint8_t id){
-	std::vector<uint8_t> packet;
-	packet.reserve(8);
-
-	/*header*/
-	packet.push_back(0xFA);
-	packet.push_back(0xAF);
-
-	/*servo id*/
-	packet.push_back(id);
-	/*flags*/
-	packet.push_back(kFlag_WRITEFLASHROM);
-	/*address*/
-	packet.push_back(0xFF);
-	/*length*/
-	packet.push_back(0);
-	/*count*/
-	packet.push_back(0);
-	/*sum*/
-	packet.push_back(0);
-	for(uint8_t i = 2;i < (packet.size() - 1);++i)
-		packet.back() ^= packet.at(i);
-
-	servoWriteRead_.write(true);
-	HAL_UART_Transmit(uart_ , packet.data(), (uint16_t)packet.size(), 100);
-	servoWriteRead_.write(false);
+	SpecialTransmissionData(id , flags::WRITE_FLASH_ROM, memory_adr::SPECIAL_OPERATION);
 }
+
+void RS40xCB::reboot(uint8_t id){
+	SpecialTransmissionData(id, flags::REBOOT, memory_adr::SPECIAL_OPERATION);
+}
+
+void RS40xCB::change_id(uint8_t old_id, uint8_t new_id, std::vector<uint8_t> sendvector_new_id){
+	sendvector_new_id.push_back(new_id);
+	SendDataOfShortpacket(old_id, memory_adr::ID, sendvector_new_id);
+	sendvector_new_id.clear();
+	HAL_Delay(300);
+	write_rom(new_id);
+	HAL_Delay(1200);
+}
+
+
